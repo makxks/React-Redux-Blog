2825be51b174ae459ce3f48e3e38c6cb7bc79bea
Maximilian G. Platt <maxplatt7@gmail.com>
2017-04-05 11:22:52 +0800

    remove model change actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,3 @@
-import { Post } from '../models/post.model';
-
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
 export const FETCH_POST = 'FETCH_POST';
--- src/models/post.model.js
+++
@@ -1,8 +0,0 @@
-export class Post {
-    constructor(title,
-        categories,
-        content,
-        timePosted,
-        comments,
-        id) {}
-}
